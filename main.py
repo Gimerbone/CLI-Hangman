3fa1e8eb23b66cdd6619a9ba0e5f77e9167e5b14
Gimerbone <133675330+Gimerbone@users.noreply.github.com>
2025-01-17 10:33:41 +0700

    Publishing v1.2.0

--- main.py
+++ main.py
@@ -1,120 +1,26 @@
 import random as ran
+import word_list as wl
+import ascii_art as renderer
 import time
-
-title = r"""
-=================================================
-  _                                            
- | |                                            
- | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
- | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
- | | | | (_| | | | | (_| | | | | | | (_| | | | |
- |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    |___/
-
-=================================================
-
-Created by Gimerbone, Github: https://github.com/Gimerbone
-Your game will start shortly..
-
-                 
-"""
-
-stages = [
-    r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''',
-    r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''',
-    r'''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''',
-    r'''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========
-''',
-    r'''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''',
-    r'''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''',
-    r'''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''',
-    r'''
-  +---+
-      |
-      |
-      |
-      |
-      |
-=========
-'''
-]
-
-with open("D:/Bootcamp/Udemy Python/100 Days Python/Day 7 - Hangman/word.txt", "r") as file:
-    word_list = [line.strip() for line in file]
     
-picked_word = ran.choice(word_list)
+tried_letter = ""
 chances = 7
 isPlayerWin = False
 correctIndex = []
-empty_box = len(picked_word)
-tried_letter = ""
 
+picked_word = ran.choice(wl.word_list)
+empty_box = len(picked_word)
 display_word = ""
 for i in range(0, len(picked_word)):
         display_word = display_word + "_ "
 
-print(title)
+print(renderer.title)
 time.sleep(2)
         
 while True:
         print(f"****************************{chances}/7 LIVES LEFT****************************")
         print(f"Tried Letter: {tried_letter}\n")
-        print(stages[max(0, chances)] + "\n")
+        print(renderer.stages[max(0, chances)] + "\n")
         print(display_word + "\n")
         
         if chances > 0:
